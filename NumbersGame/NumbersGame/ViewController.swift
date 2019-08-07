86e2a2a6e657c5576c11776ab256247383d0cd0b
shotaro427 <toshinobu.s111@gmail.com>
2019-08-07 15:20:16 +0900

    ランダムな値を生成する機能の追加

--- NumbersGame/NumbersGame/ViewController.swift
+++ NumbersGame/NumbersGame/ViewController.swift
@@ -2,19 +2,20 @@
 //  ViewController.swift
 //  NumbersGame
 //
-//  Created by 田内　翔太郎 on 2019/08/07.
+//  Created by 田内　翔太郎 on 2019/08/05.
 //  Copyright © 2019 田内　翔太郎. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /// ランダムな値を生成する
+    var trueNumber: Int = Int.random(in: 1...100) // Ind.random(in: ○~□)： ○〜□の間のInt型からランダムな値を生成する
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
